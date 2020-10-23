a21ebad1fac394ff22f220cad5ed2780bfe1e294
Igor Stakic <stakic@mdpi.com>
2020-10-23 15:23:18 +0200

    Add basic list item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import ListItem from './components/ListItem';
+
 function App() {
+	const [items, setItems] = useState([
+		{
+			id: 0,
+			itemName: 'null'
+			
+		}
+	])
+
 	return (
 		<div>
-
+			{
+				items.map( item => (
+					<ListItem 
+						key={item.id}
+						name={item.itemName}
+						id={item.id}
+					/>
+				))
+			}
 		</div>
 	);
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ListItem = ({
+    name, id
+}) => {
+    return (
+        <div>
+            <div></div>
+            <div>{id} - {name}</div>
+        </div>
+    )
+}
+
+export default ListItem;
